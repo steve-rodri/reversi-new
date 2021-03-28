23ae961fb2061c28d48d34b2af3e5e0e54d56675
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2021-03-28 16:01:23 -0400

    Add Chakra

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
+import { ChakraProvider } from "@chakra-ui/react";
 import store from "./redux/store";
 import App from "./components/App";
 
 const Root = () => (
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <ChakraProvider>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ChakraProvider>
 );
 
 render(<Root />, document.getElementById("root"));
