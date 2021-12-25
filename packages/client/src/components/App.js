b8784a34130f8e5fc604822e8b329b9083156e40
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2021-12-24 19:44:57 -0500

    update App.js

--- packages/client/src/components/App.js
+++ packages/client/src/components/App.js
@@ -1,24 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { Grid, useDisclosure } from "@chakra-ui/react";
-import { ChooseGameType } from "./modals";
+import { ChooseGameType, ChooseColor, EnterPlayerInfo } from "./modals";
 import Modal from "./Modal";
 import Header from "./Header";
 import Game from "./game";
 
 const App = () => {
-  const { onOpen, ...modalProps } = useDisclosure();
+  const { onOpen: openModal, ...modalProps } = useDisclosure();
   const [gameType, setGameType] = useState();
-  const [modalView, setModalView] = useState();
-  const ModalContent = props => {
-    if (modalView === "choose-game-type")
-      return <ChooseGameType setGameType={setGameType} />;
-    if (modalView === "choose-color")
-      return <ChooseColor gameType={gameType} />;
-    if (modalView === "enter-player-1") return <EnterPlayerInfo />;
-    if (modalView === "enter-player-2") return <EnterPlayerInfo />;
+  const modalContentProps = {
+    gameType,
+    setGameType,
+    closeModal: modalProps.onClose,
   };
   useEffect(() => {
-    onOpen();
+    openModal();
   }, []);
   return (
     <>
@@ -27,10 +23,20 @@ const App = () => {
         <Game />
       </Grid>
       <Modal {...modalProps} isCentered hideCloseButton>
-        <ModalContent />
+        <ModalContent {...modalContentProps} />
       </Modal>
     </>
   );
 };
 
+const ModalContent = props => {
+  const [modalView, setModalView] = useState("choose-game-type");
+  props = { ...props, setModalView };
+  if (modalView === "choose-game-type") return <ChooseGameType {...props} />;
+  if (modalView === "choose-color") return <ChooseColor {...props} />;
+  if (modalView === "enter-player-1")
+    return <EnterPlayerInfo num={1} {...props} />;
+  if (modalView === "enter-player-2")
+    return <EnterPlayerInfo num={2} {...props} />;
+};
 export default App;
