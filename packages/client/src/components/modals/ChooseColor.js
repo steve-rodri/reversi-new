9633b830b6f1e52cf6fcbadc3205e9fc2614aa9a
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2021-12-25 12:45:51 -0500

    disable Piece on ChooseColor modal if already chosen

--- packages/client/src/components/modals/ChooseColor.js
+++ packages/client/src/components/modals/ChooseColor.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import Disc from "../game/board/Disc";
 import { Grid, Center, VStack, Heading, Divider } from "@chakra-ui/react";
 import { useDispatch, useSelector } from "react-redux";
 import { playerActions } from "../../redux";
+import { turnValue } from "../../utils";
 
 export const ChooseColor = ({ gameType, setModalView, closeModal }) => {
   const dispatch = useDispatch();
@@ -29,11 +30,50 @@ export const ChooseColor = ({ gameType, setModalView, closeModal }) => {
       </Heading>
       <Divider />
       <Grid autoFlow="column" justifyContent="space-evenly" w="75%" py={10}>
-        <BlackPiece onSelect={onSelect} />
-        <WhitePiece onSelect={onSelect} />
+        <Piece onSelect={onSelect} color="black" />
+        <Piece onSelect={onSelect} color="white" />
       </Grid>
     </VStack>
   );
+};
+
+const Piece = ({ color, onSelect }) => {
+  const [disabled, setDisabled] = useState(false);
+  const players = useSelector(state => state.players);
+  const value = turnValue(color);
+  const onClick = () => {
+    if (disabled) return;
+    onSelect(color);
+  };
+  useEffect(() => {
+    players.forEach(player => {
+      if (disabled) return;
+      if (player.color === color) setDisabled(true);
+    });
+  }, [players, color]);
+  return (
+    <Center
+      bgColor="board.default"
+      opacity={disabled ? 0.4 : 1}
+      borderRadius={2}
+      boxSize={50}
+      onClick={onClick}
+    >
+      <Disc value={value} />
+    </Center>
+  );
+};
+
+ChooseColor.propTypes = {
+  gameType: PropTypes.string,
+  setModalView: PropTypes.func,
+  closeModal: PropTypes.func,
+};
+
+Piece.propTypes = {
+  color: PropTypes.string,
+  onSelect: PropTypes.func,
+  disabled: PropTypes.bool,
 };
 
 const getCurrentPlayer = gameType => {
@@ -45,46 +85,6 @@ const getCurrentPlayer = gameType => {
   if (gameType === "two-player-local") {
     return players.find(player => player.color === undefined);
   }
-};
-
-const BlackPiece = ({ onSelect }) => {
-  return (
-    <Center
-      bgColor="board.default"
-      borderRadius={2}
-      boxSize={50}
-      onClick={() => onSelect("black")}
-    >
-      <Disc value="b" />
-    </Center>
-  );
-};
-
-const WhitePiece = ({ onSelect }) => {
-  return (
-    <Center
-      bgColor="board.default"
-      borderRadius={2}
-      boxSize={50}
-      onClick={() => onSelect("white")}
-    >
-      <Disc value="w" />
-    </Center>
-  );
-};
-
-BlackPiece.propTypes = {
-  onSelect: PropTypes.func,
-};
-
-WhitePiece.propTypes = {
-  onSelect: PropTypes.func,
-};
-
-ChooseColor.propTypes = {
-  gameType: PropTypes.string,
-  setModalView: PropTypes.func,
-  closeModal: PropTypes.func,
 };
 
 export default ChooseColor;
