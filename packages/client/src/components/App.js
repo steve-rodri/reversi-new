0ec1a7f728b0d023cf1f1cbcdc85568edb231ed7
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2021-12-26 17:50:46 -0500

    move modal state to from app.js to game/index.js, add Header to App and remove Header.js file

--- packages/client/src/components/App.js
+++ packages/client/src/components/App.js
@@ -1,25 +1,23 @@
-import React, { useEffect } from "react";
-import { Grid, useDisclosure } from "@chakra-ui/react";
-import PageLoadModalRouter from "./PageLoadModalRouter";
-import Modal from "./Modal";
-import Header from "./Header";
+import React from "react";
+import { Grid, Center, Heading } from "@chakra-ui/react";
 import Game from "./game";
 
 export const App = () => {
-  const { onOpen: openModal, ...modalProps } = useDisclosure();
-  useEffect(() => {
-    openModal();
-  }, []);
   return (
     <>
       <Grid h="100vh" templateRows="auto 1fr">
         <Header />
         <Game />
       </Grid>
-      <Modal {...modalProps} isCentered hideCloseButton>
-        <PageLoadModalRouter closeModal={modalProps.onClose} />
-      </Modal>
     </>
+  );
+};
+
+const Header = () => {
+  return (
+    <Center>
+      <Heading variant="title">Reversi</Heading>
+    </Center>
   );
 };
 
