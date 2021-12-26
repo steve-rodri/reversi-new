65c93233bab80888b18a61b17d9fa78d11961c39
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2021-12-26 13:59:34 -0500

    refactor Space.js to utilize updated utils and thunks

--- packages/client/src/components/game/board/Space.js
+++ packages/client/src/components/game/board/Space.js
@@ -2,20 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 import { useDispatch, useSelector } from "react-redux";
 import { Center } from "@chakra-ui/react";
-import { selectSpace } from "../../../redux";
-import { turnValue } from "../../../utils";
+import { handleSpaceSelect } from "../../../redux";
+import { firstLetter, fullWord } from "../../../utils";
 import Disc from "./Disc";
 
 const Space = ({ value, coordinate }) => {
   const dispatch = useDispatch();
-  const turn = useSelector(({ progression }) => progression.turn);
+  const turn = useSelector(({ progression }) => firstLetter(progression.turn));
   const onSelect = e => {
     e.stopPropagation();
-    dispatch(selectSpace(coordinate, turnValue(turn)));
+    dispatch(handleSpaceSelect(coordinate, turn));
   };
   return (
     <Center onClick={onSelect} bgColor="board.default" borderRadius={2}>
-      {value ? <Disc value={value} /> : null}
+      {value ? <Disc value={fullWord(value)} /> : null}
     </Center>
   );
 };
