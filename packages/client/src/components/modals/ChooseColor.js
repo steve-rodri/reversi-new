21853fac358f8d8777062cafb3436be3b9871208
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2021-12-26 13:57:56 -0500

    refactor ChooseColor modal to use handleColorSelect thunk

--- packages/client/src/components/modals/ChooseColor.js
+++ packages/client/src/components/modals/ChooseColor.js
@@ -3,26 +3,9 @@ import PropTypes from "prop-types";
 import Disc from "../game/board/Disc";
 import { Grid, Center, VStack, Heading, Divider } from "@chakra-ui/react";
 import { useDispatch, useSelector } from "react-redux";
-import { playerActions } from "../../redux";
-import { turnValue } from "../../utils";
+import { handleColorSelect } from "../../redux";
 
-export const ChooseColor = ({ gameType, setModalView, closeModal }) => {
-  const dispatch = useDispatch();
-  const currentPlayer = getCurrentPlayer(gameType);
-  const onSelect = color => {
-    if (!currentPlayer) return;
-    dispatch(playerActions.updatePlayer({ ...currentPlayer, color }));
-    if (gameType === "one-player-local") {
-      if (color === "black") color = "white";
-      if (color === "white") color = "black";
-      dispatch(playerActions.updatePlayer({ num: 2, color }));
-      closeModal();
-    }
-    if (gameType === "two-player-local") {
-      if (currentPlayer.num === 1) setModalView("enter-player-2");
-      if (currentPlayer.num === 2) closeModal();
-    }
-  };
+export const ChooseColor = props => {
   return (
     <VStack spacing={8}>
       <Heading as="h3" fontSize="2xl">
@@ -30,34 +13,35 @@ export const ChooseColor = ({ gameType, setModalView, closeModal }) => {
       </Heading>
       <Divider />
       <Grid autoFlow="column" justifyContent="space-evenly" w="75%" py={10}>
-        <Piece onSelect={onSelect} color="black" />
-        <Piece onSelect={onSelect} color="white" />
+        <Piece {...props} value="black" />
+        <Piece {...props} value="white" />
       </Grid>
     </VStack>
   );
 };
 
-const Piece = ({ color, onSelect }) => {
+const Piece = ({ value, ...rest }) => {
+  const dispatch = useDispatch();
   const [disabled, setDisabled] = useState(false);
   const players = useSelector(state => state.players);
-  const value = turnValue(color);
-  const onClick = () => {
+  const onSelect = e => {
+    e.stopPropagation();
     if (disabled) return;
-    onSelect(color);
+    dispatch(handleColorSelect({ ...rest, color: value }));
   };
   useEffect(() => {
     players.forEach(player => {
       if (disabled) return;
-      if (player.color === color) setDisabled(true);
+      if (player.color === value) setDisabled(true);
     });
-  }, [players, color]);
+  }, [players, value]);
   return (
     <Center
       bgColor="board.default"
       opacity={disabled ? 0.4 : 1}
       borderRadius={2}
       boxSize={50}
-      onClick={onClick}
+      onClick={onSelect}
     >
       <Disc value={value} />
     </Center>
@@ -71,20 +55,7 @@ ChooseColor.propTypes = {
 };
 
 Piece.propTypes = {
-  color: PropTypes.string,
-  onSelect: PropTypes.func,
-  disabled: PropTypes.bool,
-};
-
-const getCurrentPlayer = gameType => {
-  const players = useSelector(state => state.players);
-  if (!players.length) return;
-  if (players.length === 1) return players[0];
-  if (gameType === "one-player-local")
-    return players.find(player => player.name === "Player");
-  if (gameType === "two-player-local") {
-    return players.find(player => player.color === undefined);
-  }
+  value: PropTypes.string,
 };
 
 export default ChooseColor;
