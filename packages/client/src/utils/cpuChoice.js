5a05d7d4c78d350472ba23027ef3cbf6a235676c
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2021-12-26 13:48:39 -0500

    refactor cpuChoice to accept possbileMoves as a param instead of redux state

--- packages/client/src/utils/cpuChoice.js
+++ packages/client/src/utils/cpuChoice.js
@@ -1,10 +1,9 @@
-import { getPossibleMoves } from "./checkForMove";
 import shuffle from "./shuffle";
 
-export const cpuChoice = state => {
-  const possibleMoves = shuffle(getPossibleMoves(state));
-  const randomIndex = Math.floor(Math.random() * possibleMoves.length);
-  return possibleMoves[randomIndex];
+export const cpuChoice = possibleMoves => {
+  const shuffledMoves = shuffle(possibleMoves);
+  const randomIndex = Math.floor(Math.random() * shuffledMoves.length);
+  return shuffledMoves[randomIndex];
 };
 
 export default cpuChoice;
