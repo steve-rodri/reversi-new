7e90f23cb5136cd5a54a2738d3230d38ebe8a2e0
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2021-12-26 13:59:10 -0500

    Disc component receives a fullWord colorValue to use on bgColor

--- packages/client/src/components/game/board/Disc.js
+++ packages/client/src/components/game/board/Disc.js
@@ -3,11 +3,7 @@ import PropTypes from "prop-types";
 import { Circle } from "@chakra-ui/react";
 
 const Disc = ({ value }) => {
-  const bgColor = (() => {
-    if (value === "w") return "white";
-    if (value === "b") return "black";
-  })();
-  return <Circle bgColor={bgColor} h="75%" w="75%" />;
+  return <Circle bgColor={value} h="75%" w="75%" />;
 };
 
 Disc.propTypes = {
