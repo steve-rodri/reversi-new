bbc1c4e68a9535ae9f3ca1e9d0bec814d60d0f3b
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2021-12-26 13:46:16 -0500

    add getCPU util

--- packages/client/src/utils/index.js
+++ packages/client/src/utils/index.js
@@ -2,6 +2,7 @@ export * from "./checkForMove";
 export * from "./cpuChoice";
 export * from "./findOpponentDiscs";
 export * from "./findWinner";
+export * from "./getCPU";
 export * from "./opponent";
 export * from "./shuffle";
 export * from "./turnConversion";
