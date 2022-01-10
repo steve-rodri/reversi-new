90de315b6f620f429ceffe6e2eb8bbd1ef646d20
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2022-01-10 16:13:40 -0500

    add discMount Transition

--- packages/client/src/components/game/board/Space.js
+++ packages/client/src/components/game/board/Space.js
@@ -1,22 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { useDispatch, useSelector } from "react-redux";
+import { animated, useTransition } from "react-spring";
 import { Center } from "@chakra-ui/react";
+import { getCurrentPlayer, fullWord } from "../../../utils";
 import { handleSpaceSelect } from "../../../redux";
-import { fullWord, getCurrentPlayer } from "../../../utils";
 import Disc from "./Disc";
 
-const Space = ({ value, coordinate }) => {
+const Space = ({ coordinate, value }) => {
   const dispatch = useDispatch();
   const state = useSelector(state => state);
+  const discMountTransition = useDiscMountTransition(!!value);
   const onSelect = e => {
     e.stopPropagation();
     const player = getCurrentPlayer(state);
     dispatch(handleSpaceSelect({ coordinate, player }));
   };
+  const AnimatedDisc = animated(Disc);
   return (
     <Center onClick={onSelect} bgColor="board.default" borderRadius={2}>
-      {value ? <Disc value={fullWord(value)} /> : null}
+      {discMountTransition(
+        (style, item) =>
+          item && <AnimatedDisc value={fullWord(value)} style={style} />
+      )}
     </Center>
   );
 };
@@ -24,6 +30,13 @@ const Space = ({ value, coordinate }) => {
 Space.propTypes = {
   value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
   coordinate: PropTypes.object.isRequired,
+};
+
+const useDiscMountTransition = value => {
+  return useTransition(value, {
+    from: { y: -50, scale: 1.25 },
+    enter: { y: 0, scale: 1.0 },
+  });
 };
 
 export default Space;
