42a02dd392c39e2d5b8ff392517b1b230bf0a8da
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2022-01-10 16:12:49 -0500

    refactor Disc

--- packages/client/src/components/game/board/Disc.js
+++ packages/client/src/components/game/board/Disc.js
@@ -2,8 +2,8 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Circle } from "@chakra-ui/react";
 
-const Disc = ({ value }) => {
-  return <Circle bgColor={value} h="75%" w="75%" />;
+const Disc = ({ value, ...rest }) => {
+  return <Circle bgColor={value} h="75%" w="75%" {...rest} />;
 };
 
 Disc.propTypes = {
@@ -11,3 +11,7 @@ Disc.propTypes = {
 };
 
 export default Disc;
+
+//   return flipTransition(
+//     (props, item) =>
+//       item && <AnimatedCircle style={props} bgColor={value} h="75%" w="75%" />
